b4c88fa6e9421ee00b2d268e1b249c03d2190846
soo.kim <carlsagan96@gmail.com>
2020-11-25 22:04:21 +0900

    moving ball with some flaws

--- src/index.js
+++ src/index.js
@@ -1,17 +1,154 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function plusOrMinus() {
+  return Math.round(Math.random()) * 2 - 1;
+}
+
+class Ball {
+  constructor(stageWidth, stageHeight, radius, speed) {
+    this.radius = radius;
+    this.vx = plusOrMinus() * speed;
+    this.vy = plusOrMinus() * speed;
+
+    this.x = (stageWidth - radius * 2) * Math.random() + radius;
+    this.y = (stageHeight - radius * 2) * Math.random() + radius;
+  }
+
+  draw(ctx, stageWidth, stageHeight, block) {
+    this.x += this.vx;
+    this.y += this.vy;
+
+    this.bounceWindow(stageWidth, stageHeight);
+
+    this.bounceBlock(block);
+
+    ctx.fillStyle = '#fdd700';
+    ctx.beginPath();
+    ctx.arc(this.x, this.y, this.radius, 0, 2 * Math.PI);
+    ctx.fill();
+  }
+
+  bounceWindow(stageWidth, stageHeight) {
+    const minX = this.radius;
+    const maxX = stageWidth - this.radius;
+    const minY = this.radius;
+    const maxY = stageHeight - this.radius;
+
+    if (this.x <= minX || this.x >= maxX) {
+      this.vx *= -1;
+      this.x += this.vx;
+    }
+    if (this.y <= minY || this.y >= maxY) {
+      this.vy *= -1;
+      this.y += this.vy;
+    }
+  }
+
+  bounceBlock(block) {
+    const minX = block.x - this.radius;
+    const maxX = block.maxX + this.radius;
+    const minY = block.y - this.radius;
+    const maxY = block.maxY + this.radius;
+
+    if (this.x > minX && this.x < maxX && this.y > minY && this.y < maxY) {
+      const x1 = Math.abs(minX - this.x);
+      const x2 = Math.abs(this.x - maxX);
+      const y1 = Math.abs(minY - this.y);
+      const y2 = Math.abs(this.y - minY);
+      const min1 = Math.min(x1, x2);
+      const min2 = Math.min(y1, y2);
+      const min = Math.min(min1, min2);
+
+      if (min == min1) {
+        this.vx *= -1;
+        this.x += this.vx;
+      } 
+      if (min == min2) {
+        this.vy *= -1;
+        this.y += this.vy;
+      }
+    }
+  }
+}
+
+class Block {
+  constructor(width, height, x, y) {
+    this.width = width;
+    this.height = height;
+    this.x = x;
+    this.y = y;
+    this.maxX = width + x;
+    this.maxY = height + y;
+  }
+
+  draw(ctx) {
+    const xGap = 80;
+    const yGap = 60;
+
+    ctx.fillStyle = '#ff384e';
+    ctx.beginPath();
+    ctx.rect(this.x, this.y, this.width, this.height);
+    ctx.fill();
+
+    ctx.fillStyle = '#190f3a';
+    ctx.beginPath();
+    ctx.moveTo(this.maxX, this.maxY);
+    ctx.lineTo(this.maxX - xGap, this.maxY + yGap);
+    ctx.lineTo(this.x - xGap, this.maxY + yGap);
+    ctx.lineTo(this.x, this.maxY);
+    ctx.fill();
+
+    ctx.fillStyle = '#9d0919';
+    ctx.beginPath();
+    ctx.moveTo(this.x, this.y);
+    ctx.lineTo(this.x, this.maxY);
+    ctx.lineTo(this.x - xGap, this.maxY + yGap);
+    ctx.lineTo(this.x - xGap, this.y + yGap);
+    ctx.fill();
+  }
+}
+
+function main() {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('2d');
+
+  const stageWidth = document.body.clientWidth;
+  const stageHeight = document.body.clientHeight;
+  canvas.width = stageWidth * 2;
+  canvas.height = stageHeight * 2;
+  ctx.scale(2, 2);
+
+  document.body.appendChild(canvas);
+
+  const ball = new Ball(stageWidth, stageHeight, 60, 5);
+  const block = new Block(350, 30, 150, 225);
+
+  function animate(t) {
+    window.requestAnimationFrame(animate);
+    ctx.clearRect(0, 0, stageWidth, stageHeight);
+
+    block.draw(ctx);
+    ball.draw(ctx, stageWidth, stageHeight, block);
+  }
+  
+  window.requestAnimationFrame(animate);
+}
+
+main();
+
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// reportWebVitals();
